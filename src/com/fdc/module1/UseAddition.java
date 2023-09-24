4c8044354a60309e7921a0749c296e4b27782d90
rsenthil75 <15950313+rsenthil75@users.noreply.github.com>
2023-09-24 15:17:16 -0500

    First time commit

--- src/com/fdc/module1/UseAddition.java
+++ src/com/fdc/module1/UseAddition.java
@@ -5,7 +5,7 @@ public class UseAddition {
     public static void main(String[] args) {
         Addition a1=new Addition();
         a1.setX(10);
-        a1.setY(20);
+        a1.setY(30);
        int sum= a1.addition(a1.getX(),a1.getY());
         System.out.println("Sum="+sum);
 
